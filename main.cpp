87a1e9c9c4720c025254ac8fc6b15b40da457061
JohanEspitia <71570769+JohanEspitia24@users.noreply.github.com>
2024-04-04 16:29:54 +0000

    Iniciamos los comentarios

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
-#include <iostream>
+// Inicializamos las librerias
 #include "Funciones.h"
 #include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-int main(){
-    list<string> listaPalabras;
-    list<string> listaPalabrasInv;
-    Nodo Arbol;
-    Nodo ArbolInv;
-    Arbol.setRaizAct('.');
-    ArbolInv.setRaizAct('.');
-    pedirComando(listaPalabras, listaPalabrasInv, Arbol, ArbolInv);
+int main() {
+  list<string> listaPalabras;
+  list<string> listaPalabrasInv;
+  Nodo Arbol;
+  Nodo ArbolInv;
+  Arbol.setRaizAct('.');
+  ArbolInv.setRaizAct('.');
+  pedirComando(listaPalabras, listaPalabrasInv, Arbol, ArbolInv);
 }
